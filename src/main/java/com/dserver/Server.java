7e17ac1ba1a90bc226a012acdf755d931cef573d
Renjith G R <grenjithr@gmail.com>
2016-01-17 00:57:25 +0530

    Simple TCP server is working

--- src/main/java/com/dserver/Server.java
+++ src/main/java/com/dserver/Server.java
@@ -1,7 +1,23 @@
 package com.dserver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
    public static void main(String... args) {
        System.out.println("Server is starting..");
+       try {
+           ServerSocket serverSocket = new ServerSocket(8090);
+           while (true) {
+               Socket clientSocket = serverSocket.accept();
+               BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+               bufferedReader.lines().forEach(line -> System.out.println(line));
+           }
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
    }
 }
