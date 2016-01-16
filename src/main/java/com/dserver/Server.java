09243e655d100b3b294d714333fded9f71c207b5
Renjith G R <grenjithr@gmail.com>
2016-01-17 00:41:47 +0530

    Adding main class for now

---
+++ src/main/java/com/dserver/Server.java
@@ -0,0 +1,7 @@
+package com.dserver;
+
+public class Server {
+   public static void main(String... args) {
+       System.out.println("Server is starting..");
+   }
+}
---
+++ src/test/java/com/dserver/ServerTest.java
@@ -0,0 +1,7 @@
+package com.dserver;
+
+import junit.framework.TestCase;
+
+public class ServerTest extends TestCase {
+
+}
